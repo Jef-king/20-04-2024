e8a14ce82ba662379e545fd53581f61e6fbccd84
jefking <jefking@outlook.in>
2024-04-20 21:28:33 +0530

    js almost done need to fix bug

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let count=document.querySelector(".countNo").innerText;
+let count=document.querySelector(".countNo");
+let clear=document.querySelector(".clear");
+let result=+count.innerText;
+console.log(result);
+document.querySelector('.increment').addEventListener('click',()=>{
+    ++result;
+    count.textContent=result;
+    clear.style.display='inline-block';
+})
+document.querySelector('.decrement').addEventListener('click',()=>{
+    if(result == 0){
+        clear.style.display='none';
+        document.querySelector('.error').style.display='block';
+        document.querySelector('.decrement').setAttribute(disabled,true);
+    }
+    --result;
+    count.textContent=result;
+})
+clear.addEventListener('click',()=>{
+    result=0;
+    count.textContent=result;
+    clear.style.display='none';
+})
